39b6b7e5d60cdd4d9cdb9dfb61b0c8a151c79728
ruizhili518 <ruizhili518@gmail.com>
2025-01-18 07:36:20 -0800

    Modify some image and style of Desktop.

--- src/pages/MobilePage/MobilePage.jsx
+++ src/pages/MobilePage/MobilePage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React ,{ useState, useEffect } from 'react';
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import ScreenShot from  "../../assets/screenshot.jpg";
+import ScreenShotD from "../../assets/screenshot_d.png";
 import './MobilePage.scss'
 
 const MobilePage = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const getImageSource = () => {
+        return windowWidth < 1280 ? ScreenShot : ScreenShotD;
+    };
+
     return (
         <div className="mobile-page">
             <Header />
         <div className="mobile-page__container">
-            <img src={ScreenShot} alt="Screenshot" className="mobile-page__screenshot"/>
+            <img src={getImageSource()} alt="Screenshot" className="mobile-page__screenshot"/>
             <Footer />
         </div>
     </div>
